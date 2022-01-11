7fd482550b11a59008362ee4cab8516e2db43693
florian <ffoezon@gmail.com>
2022-01-11 14:18:50 +0100

    antichrono

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -38,7 +38,7 @@ describe("Given I am connected as an employee", () => {
       expect(screen.getByTestId("icon-window").classList.contains("active-icon"))
     })
 
-    test("Then bills should be ordered from earliest to latest", () => {
+    test("Then bills should be ordered from latest to earliest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
